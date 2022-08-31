35a047bb3360c06c935862764ebe82be5fc10797
peter6053 <peternjuguna76@gmail.com>
2022-08-31 18:07:28 +0300

    cleanup

--- app/src/main/java/com/example/myapplication/quizactivity.kt
+++ app/src/main/java/com/example/myapplication/quizactivity.kt
@@ -1,26 +1,23 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_scroll_view.*
 
 class quizactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quizactivity
+        setContentView(
+            R.layout.activity_quizactivity
         )
         val questionlist = Constants.getQuestions()
-        Log.i("sixe","${questionlist.size}")
+        Log.i("sixe", "${questionlist.size}")
         val currentposition = 1
-        val question: Question? = questionlist[ currentposition -1]
+        val question: Question? = questionlist[currentposition - 1]
 
         progress_dilaog.progress = currentposition
-
-
-
-
-
+        tv_progress.text = question!!.question
 
 
     }
