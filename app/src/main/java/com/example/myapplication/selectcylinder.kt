aaafe4227a6ea6fad63e2d135021030afda27eba
peter6053 <peternjuguna76@gmail.com>
2022-09-29 12:48:43 +0300

    personal details activity

--- app/src/main/java/com/example/myapplication/selectcylinder.kt
+++ app/src/main/java/com/example/myapplication/selectcylinder.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_gashomepage.*
+import kotlinx.android.synthetic.main.activity_gashomepage.btn_continue
+import kotlinx.android.synthetic.main.activity_selectcylinder.*
 
 class selectcylinder : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selectcylinder)
+        btn_order.setOnClickListener {
+
+            val intent = Intent(this, personadetailone::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
